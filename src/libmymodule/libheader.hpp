08da008e3c1a4344444811ffb2c7ab568a01fa1a
Michael <m.aichmueller@gmail.com>
2021-11-28 00:28:23 +0000

    added macro for cross compile

--- src/libmymodule/libheader.hpp
+++ src/libmymodule/libheader.hpp
@@ -1,7 +1,27 @@
 
-
 #ifndef TESTPKG_LIBHEADER_HPP
 #define TESTPKG_LIBHEADER_HPP
+
+#if(OS == LINUX || OS == MAC) && COMPILER_VERSION >= 4000 \
+   && (COMPILER == ICC_COMPILER || COMPILER == GCC_COMPILER)
+#ifndef EXPORT
+#define EXPORT __attribute__((visibility("default")))
+#endif
+#ifndef IMPORT
+#define IMPORT __attribute__((visibility("default")))
+#endif
+#ifndef LOCAL
+#define LOCAL __attribute__((visibility("hidden")))
+#endif
+#elif(OS == WINDOWS)
+#define EXPORT __declspec(dllexport)
+#define IMPORT __declspec(dllimport)
+#define LOCAL
+#else
+#define EXPORT
+#define IMPORT
+#define LOCAL
+#endif
 
 class __declspec(dllexport) MyLibClass {
   void f();
